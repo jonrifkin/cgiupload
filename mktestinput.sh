1f62e064c6cab8bcd008555304c4a60e021eb68a
Jon Rifkin <jon.rifkin@uconn.edu>
2022-10-05 12:23:23 -0400

    Code working.  Remove debugging messsages.  Include script for creating test input

---
+++ mktestinput.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+        cat<<EOM
+   Usage: mktestinput.sh  FILENAME FILETYPE [ FILENAME FILETYPE [ ... ] ]
+EOM
+        exit
+fi
+
+BOUNDARY=-----------------------------7034497701176429139758495468
+
+#
+#  Wrap each binary file with boundary and HTTP headers and write to stdout
+#
+while [ $# -gt 0 ]
+do
+        FNAME=$1
+        shift
+        FTYPE=$1
+        shift
+cat<<EOM
+$BOUNDARY
+Content-Disposition: form-data; name="upload"; filename="$FNAME"
+Content-Type: $FTYPE
+
+EOM
+        cat $FNAME
+done
+
+
+#
+#  Write final boundary line
+#
+
+cat<<EOM
+
+$BOUNDARY--
+EOM
